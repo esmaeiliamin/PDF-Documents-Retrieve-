f17ac4cdb085883833966da64e8243bcdc79483f
Amin <aminesmaeili439@gmail.com>
2026-02-27 10:32:19 +0330

    add new

--- src/ingest.py
+++ src/ingest.py
@@ -9,3 +9,21 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 
 
 # Function to load pdf
+def load_documents():
+    # To Load the PDF documents from source documents directory
+    loader = PyPDFLoader('SOURCE_DOCUMENTS/...')
+    docs = loader.load()
+    return docs
+
+
+@click.command()
+@click.option('--device_type', default='cuda', help='select gpu or cpu for execution')
+def main(device_type,):
+    if device_type in ['cpu', 'CPU']:
+        device='cpu'
+    else:
+        device='cuda'
+    # To load the documents and split it into chunks
+    print(f"Loading documents from Source Directory")
+    
+
